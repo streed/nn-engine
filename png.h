2efe94367a5a456d0be7f845e406a4d94458a1a1
Sean reed <sean@threadloom.com>
2020-09-25 16:44:57 -0700

    This is third party code copied, copyright in code

---
+++ png.h
@@ -0,0 +1,18 @@
+#ifndef __PNG__
+#define __PNG__
+
+#include <vector>
+
+int decodePNG(std::vector<unsigned char>& out_image_32bit,
+              unsigned long& image_width,
+              unsigned long& image_height,
+              const std::vector<unsigned char>& in_png);
+
+int decodePNG(std::vector<unsigned char>& out_image,
+              unsigned long& image_width,
+              unsigned long& image_height,
+              const unsigned char* in_png,
+              size_t in_size,
+              bool convert_to_rgba32);
+
+#endif;
