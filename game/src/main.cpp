3117fee6038cc37d0a5d1bc8d1b3e9c92873e474
= <sean@reed.pub>
2020-10-04 23:52:26 -0700

    The engine is built as a lib which the game now links

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <math.h>
+#include <memory>
+
+#ifdef __APPLE__
+#include <SDL.h>
+#else
+#include <SDL2/SDL.h>
+#endif
+
+#include <engine/engine.h>
+#include "engine/config.h"
+#include "entities.h"
+#include "systems/system.h"
+#include "systems/physics_system.h"
+#include "scene/scene_state_machine.h"
+#include "scenes/basic_example_scene.h"
+#include "coordinator.h"
+#include "world.h"
+
 using namespace std;
 
 int main(int argc, char **args) {
-  cout << "Wenk!" << endl;
+  NN::Config config(argc, args);
+  NN::Engine engine(&config);
+
+  NN::Scenes::SceneStateMachine sceneStateMachine;
+  sceneStateMachine
+    .add(std::shared_ptr<NN::Scenes::Examples::BasicExampleScene>(
+          new NN::Scenes::Examples::BasicExampleScene(engine)));
+
+  engine.setSceneStateMachine(&sceneStateMachine);
+
+  engine.setup();
+  engine.run();
+
   return 0;
 }
